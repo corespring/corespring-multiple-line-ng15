5b9640f4fc87448c6db9571f17cd6e2b0518d159
rbokel <rbokel.github@qlod.org>
2016-12-01 12:53:25 +0100

    move to corespring

---
+++ docs/demo/controllers/entry.js
@@ -0,0 +1,2 @@
+exports['corespring-multiple-line-ng15'] = require('corespring-multiple-line-ng15-controller');
+exports['corespring-multiple-line-ng15'].version =  '../node_modules/corespring-multiple-line-ng15/controller';
---
+++ docs/demo/controllers/webpack.config.js
@@ -0,0 +1,18 @@
+ 
+  module.exports = {
+  "context": "/home/rbokel/workspace/pie/ng15/corespring-multiple-line-ng15/docs/demo/controllers",
+  "entry": "./entry.js",
+  "output": {
+    "path": "/home/rbokel/workspace/pie/ng15/corespring-multiple-line-ng15/docs/demo",
+    "filename": "controllers.js",
+    "library": "e882d78f06f69baaeddc8e4942593915",
+    "libraryTarget": "umd"
+  },
+  "resolve": {
+    "root": "/home/rbokel/workspace/pie/ng15/corespring-multiple-line-ng15/docs/demo/controllers/node_modules"
+  },
+  "resolveLoader": {
+    "root": "/home/rbokel/workspace/pie/ng15/corespring-multiple-line-ng15/docs/demo/controllers/node_modules"
+  }
+};
+  
