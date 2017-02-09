641b15a4ae3fc860254532ab8729fa5a674cda8d
Ed Eustace <ed.eustace@gmail.com>
2017-02-09 09:45:00 +0000

    clean up

--- docs/demo/controllers/entry.js
+++
@@ -1,2 +0,0 @@
-exports['corespring-multiple-line-ng15'] = require('corespring-multiple-line-ng15-controller');
-exports['corespring-multiple-line-ng15'].version =  '../node_modules/corespring-multiple-line-ng15/controller';
